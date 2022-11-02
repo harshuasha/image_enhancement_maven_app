cd74f27319b400bd012a04fa2ced13590b73feaf
harshitha <harshitha09asha@gmail.com>
2022-11-02 19:54:13 +0530

    Image enhancement maven app
    Signed-off-by: harshitha <harshitha09asha@gmail.com>

---
+++ enhancement_app/enhancement_app/src/main/java/com/image/enhancement_app/EnhancementAppApplication.java
@@ -0,0 +1,13 @@
+package com.image.enhancement_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnhancementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnhancementAppApplication.class, args);
+	}
+
+}
---
+++ enhancement_app/enhancement_app/src/test/java/com/image/enhancement_app/EnhancementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.image.enhancement_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnhancementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
